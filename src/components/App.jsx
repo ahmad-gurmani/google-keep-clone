5f6f7efbb47b74bca032e4df222551e4b89ebcf2
Ahmad-Gurmani <ahmad.gurmani06@gmail.com>
2023-04-13 00:54:00 +0500

    final update and completed project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
-    // console.log(Note);
+    const [notes, setNotes] = useState([]);
+
+    function addNote(newNote) {
+        setNotes(prevNotes => {
+            return [...prevNotes, newNote];
+        });
+    }
+
+    function deleteNote(id) {
+        setNotes(prevNotes => {
+            return prevNotes.filter((noteItem, index) => {
+                return index !== id;
+            });
+        });
+    }
+
     return (
         <div>
             <Header />
-            {notes.map((noteItem) =>
-                <Note
-                    key={noteItem.key}
-                    title={noteItem.title}
-                    content={noteItem.content}
-                />
-            )}
+            <CreateArea onAdd={addNote} />
+            {notes.map((noteItem, index) => {
+                return (
+                    <Note
+                        key={index}
+                        id={index}
+                        title={noteItem.title}
+                        content={noteItem.content}
+                        onDelete={deleteNote}
+                    />
+                );
+            })}
             <Footer />
         </div>
     );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 
 function Note(props) {
-    const currentYear = new Date().getFullYear();
+    function handleClick() {
+        props.onDelete(props.id);
+    }
 
     return (
         <div className="note">
             <h1>{props.title}</h1>
             <p>{props.content}</p>
+            <button onClick={handleClick}>DELETE</button>
         </div>
-    )
+    );
 }
 
 export default Note;
