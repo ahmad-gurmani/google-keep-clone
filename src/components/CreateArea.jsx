3bd03399b2145716ed276b764aaf33736d3cc3de
Ahmad-Gurmani <ahmad.gurmani06@gmail.com>
2023-04-14 01:03:59 +0500

    adding icons & Styling the Keeper App

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
+
+
+
+
 
 function CreateArea(props) {
+    const [isExpanded, setExpanded] = useState(false);
     const [note, setNote] = useState({
         title: "",
         content: ""
@@ -26,23 +34,31 @@ function CreateArea(props) {
         event.preventDefault();
     }
 
+    function expand() {
+        setExpanded(true);
+    }
+
     return (
         <div>
-            <form>
-                <input
+            <form className="create-note">
+                {isExpanded && (<input
                     name="title"
                     onChange={handleChange}
                     value={note.title}
                     placeholder="Title"
                 />
+                )}
                 <textarea
                     name="content"
+                    onClick={expand}
                     onChange={handleChange}
                     value={note.content}
                     placeholder="Take a note..."
-                    rows="3"
+                    rows={isExpanded ? 3 : 1}
                 />
-                <button onClick={submitNote}>Add</button>
+                <Zoom in={isExpanded}>
+                    <Fab onClick={submitNote}><AddIcon /></Fab>
+                </Zoom>
             </form>
         </div>
     );
